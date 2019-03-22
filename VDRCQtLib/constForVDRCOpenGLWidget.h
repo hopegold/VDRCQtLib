5e1cd6ed79cc7e1e8b87f1c4dfff1c48ab432c34
Jehyun Cha <dehan2@naver.com>
2019-03-22 15:57:53 +0900

    190322

--- VDRCQtLib/constForVDRCOpenGLWidget.h
+++ VDRCQtLib/constForVDRCOpenGLWidget.h
@@ -26,3 +26,18 @@ const GLushort STIPPLE_PATTERN = 0xAAAA;
 const float CONE_HEIGHT = 1;
 const float CONE_BASE_RADIUS = 0.5;
 const float SPEED_RATIO_FOR_DRAWING_CONE = 100;
+
+static string translate_to_window_path(const QString& QfilePath)
+{
+	string filePath = QfilePath.toLocal8Bit();
+
+	size_t i = filePath.find('/');
+	while (i != string::npos)
+	{
+		string part1 = filePath.substr(0, i);
+		string part2 = filePath.substr(i + 1);
+		filePath = part1 + R"(\)" + part2; // Use "\\\\" instead of R"(\\)" if your compiler doesn't support C++11's raw string literals
+		i = filePath.find('/', i + 1);
+	}
+	return filePath;
+}
